3aa6d222cc8bd596ceb6e44197239ec98e4a6516
Saurabh Singh <itsmesaurabh214@gmail.com>
2026-06-12 06:03:57 +0530

    only local

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Auth from './Auth';
 import './App.css';
 import Editor from "@monaco-editor/react";
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = 'http://localhost:5000';
 
 
 function App() {
